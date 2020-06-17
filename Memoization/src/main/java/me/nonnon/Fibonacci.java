e41d293e335463887a44b312efda631e922ceb3f
Ha Hwang <spyderthing@gmail.com>
2020-06-16 22:46:52 -0400

    Added Fibonacci method

--- Memoization/src/main/java/me/nonnon/Fibonacci.java
+++ Memoization/src/main/java/me/nonnon/Fibonacci.java
@@ -2,5 +2,15 @@ package me.nonnon;
 
 public class Fibonacci {
 	
+	public static int fib(int n, int[] memoiz) {
+		if (n<=0) {
+			return 0;
+		} else if (n==1) {
+			return 1;
+		} else if (memoiz[n] == -1) {
+			memoiz[n] = fib(n-1, memoiz) + fib(n-2, memoiz);
+		}
+		return memoiz[n];
+	}
 
 }
